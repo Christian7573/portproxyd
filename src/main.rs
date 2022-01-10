0b901aeaf30ac4e333be5ffadb4c89f64c6a60df
Christian7573 <cag4561@gmail.com>
2022-01-10 09:51:01 -0600

    Add a verbose flag

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::{SocketAddr, TcpListener, TcpStream, Shutdown};
 
 fn main() {
-    let (listen_on, forward_to, buf_size) = {
+    let (listen_on, forward_to, buf_size, extra_debug) = {
         use clap::{App, Arg};
         let a = App::new("Port Proxy Daemon")
             .author("SirAlador")
@@ -26,6 +26,11 @@ fn main() {
                  .long("buffer-size")
                  .takes_value(true)
                  .help("The size for each communication buffer. Each active connection has two communication buffers"))
+            .arg(Arg::new("extra_debug")
+                 .short('v')
+                 .long("verbose")
+                 .help("Print MOAR stuff!")
+                 .takes_value(false))
             .get_matches();
 
         let mut listen_on = a.value_of("listen_on").unwrap().to_string();
@@ -33,8 +38,9 @@ fn main() {
         let listen_on: SocketAddr = listen_on.parse().expect("Your --listen-to address was not a valid socket address");
         let forward_to: SocketAddr = a.value_of("forward_to").unwrap().parse().expect("Your --forward-to address was not a valid socket address");
         let buf_size: usize = a.value_of("buf_size").map(|d| d.parse().expect("Your --buffer-size was not a valid number")).unwrap_or(1024 * 1024 / 2);
+        let extra_debug: bool = a.is_present("extra_debug");
 
-        (listen_on, forward_to, buf_size)
+        (listen_on, forward_to, buf_size, extra_debug)
     };
 
     let listener = TcpListener::bind(listen_on).expect("Failed to bind to listen_to address");
@@ -44,7 +50,7 @@ fn main() {
         match conn {
             Ok((conn, from_addr)) => {
                 let to_addr = forward_to.clone();
-                socket(conn, from_addr, to_addr, buf_size, id);
+                socket(conn, from_addr, to_addr, buf_size, id, extra_debug);
                 id = id + 1;
             },
             Err(err) => {
@@ -56,28 +62,28 @@ fn main() {
     }
 }
 
-fn socket(a: TcpStream, a_addr: SocketAddr, b_addr: SocketAddr, buf_size: usize, my_id: usize) {
+fn socket(a: TcpStream, a_addr: SocketAddr, b_addr: SocketAddr, buf_size: usize, my_id: usize, extra_debug: bool) {
     println!("New socket: {} with id {}", a_addr, my_id);
     match TcpStream::connect(b_addr) {
         Ok(b) => {
             let from_client = match a.try_clone() { Ok(s) => s, Err(err) => { eprintln!("Failed to clone a socket from_client\n{}", err); return; } };
             let to_server = match b.try_clone() { Ok(s) => s, Err(err) => { eprintln!("Failed to clone a socket to_server\n{}", err); return; } };
-            std::thread::spawn(move || { socket_transport(from_client, to_server, buf_size, my_id); });
+            std::thread::spawn(move || { socket_transport(from_client, to_server, buf_size, my_id, extra_debug); });
             let from_server = b;
             let to_client = a;
-            std::thread::spawn(move || { socket_transport(from_server, to_client, buf_size, my_id); });
+            std::thread::spawn(move || { socket_transport(from_server, to_client, buf_size, my_id, extra_debug); });
         },
         Err(err) => {
             eprintln!("Failed to connect to destination for socket {}\n{}", my_id, err);
         }
     }
 }
-fn socket_transport(mut a: TcpStream, mut b: TcpStream, buf_size: usize, my_id: usize) {
+fn socket_transport(mut a: TcpStream, mut b: TcpStream, buf_size: usize, my_id: usize, extra_debug: bool) {
     let mut buf = vec![0u8; buf_size];
     use std::io::{Read, Write};
     'outer: while let Ok(bytes_read) = a.read(&mut buf) {
         if bytes_read == 0 { 
-            println!("a Transport died {}", my_id);
+            if extra_debug { println!("a Transport died {}", my_id); }
             break 'outer;
         }
         let mut total_written = 0;
@@ -85,12 +91,12 @@ fn socket_transport(mut a: TcpStream, mut b: TcpStream, buf_size: usize, my_id: 
             match b.write(&buf[total_written..bytes_read]){
                 Ok(bytes_written) => { total_written += bytes_written; }
                 Err(_) => {
-                    println!("a Transport died {} 2", my_id);
+                    if extra_debug { println!("a Transport died {} 2", my_id); }
                     break 'outer;
                 }
             }
         }
     }
-    if let Err(err) = a.shutdown(Shutdown::Read) { eprintln!("Error occured while shutting down read for a transport with id {}\n{}", my_id, err); }
-    if let Err(err) = b.shutdown(Shutdown::Write) { eprintln!("Error occured while shutting down read for a transport with id {}\n{}", my_id, err); }
+    if let Err(err) = a.shutdown(Shutdown::Read) { if extra_debug { eprintln!("Error occured while shutting down read for a transport with id {}\n{}", my_id, err); } }
+    if let Err(err) = b.shutdown(Shutdown::Write) { if extra_debug { eprintln!("Error occured while shutting down read for a transport with id {}\n{}", my_id, err); } }
 }
